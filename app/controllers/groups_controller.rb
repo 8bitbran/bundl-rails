a448012ab5b7d633d758d107cae979ae8660f877
8bitbran <brandonmar97@gmail.com>
2020-10-05 10:03:33 -0400

    Add groups show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,11 @@
 class GroupsController < ApplicationController
+    def show
+        find_group
+    end
+
+    private
+
+    def find_group
+        @group = Group.find(params[:id])
+    end
 end
