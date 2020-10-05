7edcb5fd9c213e553b23119e9cab999edc151d7d
8bitbran <brandonmar97@gmail.com>
2020-10-05 09:57:19 -0400

    Add group validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,5 @@
 class Group < ApplicationRecord
+    validates :name, presence: true, length: { minimum: 5, maximum: 64 }
+    validates :description, presence: true, length: { minimum: 50, maximum: 500 }
+    validates :location, presence: true
 end
