de328da67085db485eabfffd28f693317e982699
8bitbran <brandonmar97@gmail.com>
2020-10-05 10:41:31 -0400

    Add group new and create actions

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,9 +7,22 @@ class GroupsController < ApplicationController
         find_group
     end
 
+    def new
+    end 
+
+    def create
+        @group = Group.new(group_params)
+        @group.save
+        redirect_to @group
+    end
+
     private
 
     def find_group
         @group = Group.find(params[:id])
     end
+
+    def group_params 
+        params.require(:group).permit(:name, :description, :location)
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'static#home'
-  resources :groups, only: [:index, :show]
+  resources :groups, only: [:index, :show, :new, :create]
 end
