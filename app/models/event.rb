b5322dd9f3d9821cc5eb64640e7470974f50e89a
8bitbran <brandonmar97@gmail.com>
2020-10-05 10:48:23 -0400

    Add event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 16, maximum: 64 }
+    validates :description, presence: true, length: { minimum: 50, maximum: 500 }
+    validates :location, presence: true
 end
