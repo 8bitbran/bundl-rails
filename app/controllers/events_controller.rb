ff009d166437dbaa62962b0d771a280f34243fda
8bitbran <brandonmar97@gmail.com>
2020-10-06 18:30:44 -0400

    Add events new and create actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,9 +7,26 @@ class EventsController < ApplicationController
         find_event
     end 
 
+    def new
+        @event = Event.new
+    end
+
+    def create
+        @event = Event.new(event_params)
+        if @event.save
+            redirect_to @event
+        else 
+            render :new
+        end
+    end
+
     private 
 
     def find_event
         @event = Event.find(params[:id])
+    end
+
+    def event_params
+        params.require(:event).permit(:title, :description, :location)
     end
 end
