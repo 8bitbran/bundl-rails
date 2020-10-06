0df484a4aae35227d90e0080a7910bb5f4b04991
8bitbran <brandonmar97@gmail.com>
2020-10-06 18:36:13 -0400

    Update new and create actions

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,12 +8,16 @@ class GroupsController < ApplicationController
     end
 
     def new
+        @group = Group.new
     end 
 
     def create
         @group = Group.new(group_params)
-        @group.save
-        redirect_to @group
+        if @group.save
+            redirect_to @group
+        else
+            render :new
+        end
     end
 
     private
