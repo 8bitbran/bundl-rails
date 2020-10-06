10a816d287455aa50b7260cd505e6b4035890ff2
8bitbran <brandonmar97@gmail.com>
2020-10-05 20:52:50 -0400

    Add associations to models

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,9 @@ class Group < ApplicationRecord
     validates :name, presence: true, length: { minimum: 5, maximum: 64 }
     validates :description, presence: true, length: { minimum: 50, maximum: 500 }
     validates :location, presence: true
+    has_one :topic
+    has_many :memberships
+    # has_many :members, through: :memberships, source: :user
+    has_many :events
+    has_many :rsvps, through: :events
 end
