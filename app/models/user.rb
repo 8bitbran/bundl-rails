1037fe377a32c17e72816b345914eeb9a05e243d
8bitbran <brandonmar97@gmail.com>
2020-10-17 19:35:21 -0400

    Update user and group associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  accepts_nested_attributes_for :event
-  accepts_nested_attributes_for :rsvp
-  accepts_nested_attributes_for :group
-  accepts_nested_attributes_for :membership
+  has_many :groups
 end
