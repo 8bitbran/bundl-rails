48b898674e0dbad441a8846fae76b8133699cb18
8bitbran <brandonmar97@gmail.com>
2020-10-17 19:05:06 -0400

    Add nested attributes to user model

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,6 +3,7 @@ class Group < ApplicationRecord
     validates :description, presence: true, length: { minimum: 50, maximum: 500 }
     validates :location, presence: true
     has_one :topic
+    has_one :admin, source: :user
     has_many :memberships
     has_many :members, through: :memberships, source: :user
     has_many :events
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  accepts_nested_attributes_for :event
+  accepts_nested_attributes_for :rsvp
+  accepts_nested_attributes_for :group
+  accepts_nested_attributes_for :membership
 end
