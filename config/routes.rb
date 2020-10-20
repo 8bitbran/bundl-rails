53f835bc324a0fec9a982498e7f1be6996a7f2a2
8bitbran <brandonmar97@gmail.com>
2020-10-20 19:49:34 -0400

    Add rsvps create and destroy routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'static#home'
   resources :groups, only: [:index, :show, :new, :create]
   resources :events, only: [:index, :show, :new, :create]
+  resources :rsvps, only: [:create, :destroy]
 end
