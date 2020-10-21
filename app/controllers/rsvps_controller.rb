894e465936a28b72ee02809eb70a8d84b59e04fc
8bitbran <brandonmar97@gmail.com>
2020-10-20 20:08:18 -0400

    Fix route paths

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -4,16 +4,17 @@ class RsvpsController < ApplicationController
         @rsvp = current_user.rsvps.build(rsvp_params)
         if @rsvp.save
             flash[:success] = "You RSVP'd"
-            redirect_to root
+            redirect_to root_path
         else 
             flash[:danger] = "Failed to submit RSVP"
-            redirect_to root
+            redirect_to root_path
+        end
     end
     
     def destroy
         @rsvp = Rsvp.find(params[:id])
         @rsvp.destroy
-        redirect_to root
+        redirect_to root_path
     end
 
     private
