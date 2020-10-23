84eac8c420375757a0f2a27f69471266fb27a7f9
8bitbran <brandonmar97@gmail.com>
2020-10-22 20:20:13 -0400

    Groups have nested memberships index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static#home'
-  resources :groups, only: [:index, :show, :new, :create]
+  resources :groups, only: [:index, :show, :new, :create] do
+    resources :memberships, only: [:index]
+  end
   resources :events, only: [:index, :show, :new, :create]
   resources :rsvps, only: [:create, :destroy]
   resources :memberships, only: [:create, :destroy]
