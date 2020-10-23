328c5909fefbc7fc275c9cb0a42312f8ddab13a7
8bitbran <brandonmar97@gmail.com>
2020-10-22 20:25:12 -0400

    Change memberships path to members

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static#home'
   resources :groups, only: [:index, :show, :new, :create] do
-    resources :memberships, only: [:index]
+    resources :memberships, only: [:index], path: "/members"
   end
   resources :events, only: [:index, :show, :new, :create]
   resources :rsvps, only: [:create, :destroy]
