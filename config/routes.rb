0d15437499a81bf8d4b9120503220a22df70c29d
8bitbran <brandonmar97@gmail.com>
2020-10-23 20:47:48 -0400

    Discussions nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'static#home'
   resources :groups, only: [:index, :show, :new, :create] do
     resources :memberships, only: [:index], path: "/members"
+    resources :discussions, only: [:index, :new, :create, :destroy]
   end
   resources :events, only: [:index, :show, :new, :create]
   resources :rsvps, only: [:create, :destroy]
