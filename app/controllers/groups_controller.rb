937a1082521db03cb3bfd256179888dbbb40df34
8bitbran <brandonmar97@gmail.com>
2020-11-02 19:39:52 -0500

    Add nested form for group tags

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,17 +11,26 @@ class GroupsController < ApplicationController
 
     def new
         @group = Group.new
+        @group.group_tags.build
     end 
 
     def create
         @group = Group.new(group_params)
         @group.user_id = current_user.id
+        @group.tag_list = params[:group_tags][:tag_list]
         if @group.save
             redirect_to @group
         else
             render :new
         end
     end
+
+    def destroy 
+        @group = Group.find(params[:id])
+        @group.destroy if current_user == @group.user
+        flash[:notice] = "Your group has been permanently deleted. This cannot be undone."
+        redirect_to groups_path
+    end 
 
     private
 
@@ -30,6 +39,6 @@ class GroupsController < ApplicationController
     end
 
     def group_params 
-        params.require(:group).permit(:name, :description, :location)
+        params.require(:group).permit(:name, :description, :location, group_tags_attributes: [:tag_list]) 
     end 
 end
