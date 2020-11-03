0deede676cf3e88647f6b20a59f4754a6a919fc4
8bitbran <brandonmar97@gmail.com>
2020-11-02 19:41:30 -0500

    Nested resources for events

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static#home'
-  resources :groups, only: [:index, :show, :new, :create] do
+  resources :groups, only: [:index, :show, :new, :create, :destroy] do
     resources :memberships, only: [:index], path: "/members"
     resources :discussions, only: [:index, :create, :destroy]
+    resources :events, only: [:index, :show, :new, :create]
   end
-  resources :events, only: [:index, :show, :new, :create]
+  resources :events, only: [:index]
   resources :rsvps, only: [:create, :destroy]
   resources :memberships, only: [:create, :destroy]
 end
