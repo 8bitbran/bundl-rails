8d0cd6b9ffdbfc559d36832246b72411038bd86b
8bitbran <brandonmar97@gmail.com>
2020-11-16 14:56:54 -0500

    Add group validation to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
     validates :title, presence: true, length: { minimum: 16, maximum: 64 }
     validates :description, presence: true, length: { minimum: 50, maximum: 500 }
     validates :location, presence: true
+    validates :group, presence: true
     belongs_to :group
     has_many :rsvps
     has_many :members, through: :rsvp, source: :user
