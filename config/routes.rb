9fc7168921832760e82fea049ddbe0e692201f80
8bitbran <brandonmar97@gmail.com>
2020-11-16 15:00:47 -0500

    Update groups and events routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'static#home'
   resources :searches
-  resources :groups, only: [:index, :show, :new, :create, :destroy] do
+  resources :groups do
     resources :memberships, only: [:index], path: "/members"
     resources :discussions, only: [:index, :create, :destroy]
-    resources :events, only: [:index, :show, :new, :create]
+    resources :events, only: [:index, :show, :new]
   end
-  resources :events, only: [:index]
+  resources :events, only: [:index, :create]
   resources :rsvps, only: [:create, :destroy]
   resources :memberships, only: [:create, :destroy]
 end
