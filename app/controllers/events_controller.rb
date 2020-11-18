4be2e4fd00ff789a514f23adc9f0ef2bd85dc606
8bitbran <brandonmar97@gmail.com>
2020-11-17 21:09:13 -0500

    Add find_group method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
     
     def index 
         @events = Event.all
-        @group = Group.find(params[:group_id]) if params[:group_id]
+        find_group if params[:group_id]
     end
     
     def show
@@ -11,25 +11,28 @@ class EventsController < ApplicationController
     end 
 
     def new
-        @group = Group.find(params[:group_id])
-        if @group.user == current_user
-            @event = Event.new
-        else
-            flash[:danger] = "You must be the group owner to create events."
-            redirect_to @group
-        end
+        @event = Event.new
     end
 
     def create
         @event = Event.new(event_params)
-        if @event.save
-            redirect_to group_path(@event.group)
-        else 
-            render :new
+        if @event.group.user == current_user
+            if @event.save
+                redirect_to group_path(@event.group)
+            else 
+                render :new
+            end
+        else
+            flash[:danger] = "You must be the group owner to create events."
+            redirect_to '/'
         end
     end
 
     private 
+
+    def find_group
+        @group = Group.find(params[:group_id])
+    end
 
     def find_event
         @event = Event.find(params[:id])
